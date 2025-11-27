cec706f0a8588a72ba88cf1ae0d2720a2d103c74
HttpMarco <mirco.lindenau@gmx.de>
2025-11-27 23:08:43 +0100

    Update shadowJar configuration and adjust dependencies for clarity

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,17 @@ repositories {
     }
 }
 
+tasks.shadowJar {
+    archiveClassifier.set("")
+}
+
 dependencies {
     api("io.grpc:grpc-services:1.77.0")
     api("io.grpc:grpc-netty-shaded:1.77.0")
 
     api("dev.httpmarco.polocloud:proto:3.0.0-pre.8-SNAPSHOT")
+    api("dev.httpmarco.polocloud:shared:3.0.0-pre.8-SNAPSHOT")
 
-    implementation("dev.httpmarco.polocloud:shared:3.0.0-pre.8-SNAPSHOT")
     compileOnly("com.google.code.gson:gson:2.13.2")
     compileOnly("org.jetbrains:annotations:26.0.2-1")
 }
