3f24e58b94552b2e060ce404a83667dc9bb45e00
httpmarco <httpmarco@gmx.de>
2025-11-27 01:22:42 +0100

    Add Nexus publishing configuration and update Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("java-library")
+    `maven-publish`
 }
 
 group = "dev.httpmarco.polocloud"
@@ -23,4 +25,50 @@ dependencies {
     implementation("dev.httpmarco.polocloud:shared:3.0.0-pre.8-SNAPSHOT")
     compileOnly("com.google.code.gson:gson:2.13.2")
     compileOnly("org.jetbrains:annotations:26.0.2-1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            artifact(tasks.jar.get())
+
+            pom {
+                name.set("polocloud-proto")
+                description.set("PoloCloud gRPC API with bundled dependencies")
+                url.set("https://github.com/thePolocloud/polocloud")
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Mirco Lindenau")
+                        email.set("mirco.lindenau@gmx.de")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/thePolocloud/polocloud")
+                    connection.set("scm:git:https://github.com/thePolocloud/polocloud.git")
+                    developerConnection.set("scm:git:https://github.com/thePolocloud/polocloud.git")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/releases/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+
+            username.set(System.getenv("ossrhUsername") ?: "")
+            password.set(System.getenv("ossrhPassword") ?: "")
+        }
+    }
+    useStaging.set(!project.rootProject.version.toString().endsWith("-SNAPSHOT"))
 }
