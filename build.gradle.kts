cce88fdde1e09ecfe68f171a98f1573dc03104ed
HttpMarco <mirco.lindenau@gmx.de>
2025-11-29 10:39:59 +0100

    Remove signing configuration from Maven publication setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("com.gradleup.shadow") version "9.2.2"
     id("java-library")
-    id("signing")
     `maven-publish`
 }
 
@@ -73,10 +72,6 @@ publishing {
             }
         }
     }
-}
-
-signing {
-    sign(publishing.publications["maven"])
 }
 
 nexusPublishing {
